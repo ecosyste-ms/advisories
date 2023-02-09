064f4fe5d6d95e482e098509842914c34c63e15c
Andrew Nesbitt <andrewnez@gmail.com>
2023-02-09 14:22:29 +0000

    Unique page titles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -19,4 +19,12 @@ module ApplicationHelper
   def render_markdown(str)
     CommonMarker.render_html(str, :GITHUB_PRE_LANG, [:tagfilter, :autolink, :table, :strikethrough])
   end
+
+  def meta_title
+    [@meta_title, 'Ecosyste.ms: Advisories'].compact.join(' | ')
+  end
+
+  def meta_description
+    @meta_description || 'An open API service providing security vulnerability metadata for many open source software ecosystems.'
+  end
 end

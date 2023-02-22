8d87f1e91545c2ee666d05902347f37950588506
Andrew Nesbitt <andrewnez@gmail.com>
2023-02-22 15:53:16 +0000

    More filtering and sorting options

--- app/controllers/api/v1/advisories_controller.rb
+++ app/controllers/api/v1/advisories_controller.rb
@@ -6,7 +6,17 @@ class Api::V1::AdvisoriesController < Api::V1::ApplicationController
     scope = scope.ecosystem(params[:ecosystem]) if params[:ecosystem].present?
     scope = scope.package_name(params[:package_name]) if params[:package_name].present?
 
-    @pagy, @advisories = pagy(scope)
+    scope = scope.created_after(params[:created_after]) if params[:created_after].present?
+    scope = scope.updated_after(params[:updated_after]) if params[:updated_after].present?
+
+    if params[:sort].present? || params[:order].present?
+      sort = params[:sort] || 'published_at'
+      order = params[:order] || 'desc'
+      sort_options = sort.split(',').zip(order.split(',')).to_h
+      scope = scope.order(sort_options)
+    end
+
+    @pagy, @advisories = pagy(scope.includes(:source))
   end
 
   def show

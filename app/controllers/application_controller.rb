4e8e65379996fa09979bfd01586a0f5736dc1a53
Andrew Nesbitt <andrewnez@gmail.com>
2023-05-03 11:26:34 +0100

    Add link to json api

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   include Pagy::Backend
+
+  def default_url_options(options = {})
+    Rails.env.production? ? { :protocol => "https" }.merge(options) : options
+  end
 end

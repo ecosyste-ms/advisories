f121f2c20841204eb2c8f71c142307ff2f2427ba
Andrew Nesbitt <andrewnez@gmail.com>
2023-08-23 14:49:53 +0100

    typo

--- app/models/export.rb
+++ app/models/export.rb
@@ -1,5 +1,5 @@
 class Export < ApplicationRecord
-  validates_presence_of :date, :bucket_name, :packages_count
+  validates_presence_of :date, :bucket_name, :advisories_count
 
   def download_url
     "https://#{bucket_name}.s3.amazonaws.com/packages-#{date}.tar.gz"

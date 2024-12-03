dada25d587568c0034335066d59ce0fecafe642d
Andrew Nesbitt <andrewnez@gmail.com>
2024-12-03 12:15:21 +0000

    Enable pg_stat_statements extension in the database schema

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,7 @@
 ActiveRecord::Schema[8.0].define(version: 2024_12_03_120748) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+  enable_extension "pg_stat_statements"
 
   create_table "advisories", force: :cascade do |t|
     t.bigint "source_id", null: false

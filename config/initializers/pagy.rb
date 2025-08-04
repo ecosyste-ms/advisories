9f6c70c30681c2b0558b5a77f501888a86db1a4e
Andrew Nesbitt <andrewnez@gmail.com>
2025-08-04 08:57:41 +0100

    Add missing Pagy extras and set overflow behavior

--- config/initializers/pagy.rb
+++ config/initializers/pagy.rb
@@ -1,9 +1,11 @@
 require 'pagy/extras/headers'
 require 'pagy/extras/limit'
-require 'pagy/extras/bootstrap'
 require 'pagy/extras/countless'
+require 'pagy/extras/bootstrap'
 require 'pagy/extras/array'
+require 'pagy/extras/overflow'
 
 Pagy::DEFAULT[:limit] = 100
 Pagy::DEFAULT[:limit_param] = :per_page
 Pagy::DEFAULT[:limit_max] = 1000
+Pagy::DEFAULT[:overflow] = :empty_page
